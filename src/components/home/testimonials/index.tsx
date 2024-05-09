e9e7c7e3b1d08cc765820db0bbc4d451efe352b9
Favour Adekogbe <favouradekogbe@gmail.com>
2024-05-09 18:26:57 +0100

    updated homepage testimonials

--- src/components/home/testimonials/index.tsx
+++ src/components/home/testimonials/index.tsx
@@ -9,15 +9,60 @@ const Testimonials = () => {
         Hear what our clients have to say about us{" "}
         <SmallZigzag2 className={styles.zigzag} />{" "}
       </h5>
-      <div className={styles.vidWrap}>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/aQaBjvArImM?start=1"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+      <div className={styles.videos}>
+        <div>
+          <div className={styles.vidWrap}>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/5q9GrtTbMcI?si=ZF34Ca45VcCVGVHQ"
+              title="YouTube video player"
+              // frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <p className={styles.vidTtl}>Customer Reviews</p>
+          <p className={styles.vidTxt}>
+            At Zitra Investments, we're committed to helping our customers
+            achieve success.
+          </p>
+        </div>
+        <div>
+          <div className={styles.vidWrap}>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/pVTzN69moVA?si=jR6Spy3Zh_TNalVl"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <p className={styles.vidTtl}>CEO’s Interview</p>
+          <p className={styles.vidTxt}>
+            Our CEO gives a deep dive into why you should invest with Zitra
+          </p>
+        </div>
+        <div>
+          <div className={styles.vidWrap}>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/Jbt04CRi6U4?si=3lmcBFpK2OzznYqb"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <p className={styles.vidTtl}>Investor’s Forum</p>
+          <p className={styles.vidTxt}>
+            Recap of Zitra Investor's Forum: Key Insights and Highlights
+          </p>
+        </div>
       </div>
     </section>
   );
